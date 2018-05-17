6241fa4d2590e1f5b4f114ca50863cec0ab55c86
PENCHAL G <32594705+penchal8@users.noreply.github.com>
2018-05-17 20:20:58 +0530

    Update Test.java

--- img/FinalOutput/src/com/penchal/task/four/Test.java
+++ img/FinalOutput/src/com/penchal/task/four/Test.java
@@ -2,8 +2,9 @@ package com.penchal.task.four;
 //import com.penchal.task.TextFileTest;
 public class Test{
 	public static void main(String[] args) {
-		
-		TextFileTest.modifyFile("/home/penchal/Desktop/file.txt", "Hello boss", "Hi Guest","penchal.docx");
+		//example: TextFileTest.modifyFile("/home/penchal/Desktop/file.txt", "Hello boss", "Hi Guest","/home/penchal/fileupdate.docx");
+        
+		TextFileTest.modifyFile("filePath", "Hello boss", "Hi Guest","createNewFilePath");
         
         System.out.println("Done.");
 	}
